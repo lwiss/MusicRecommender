bb8acf29ab1259777a83ffc8a24c829db15da549
lwiss <wissem.allouchi@epfl.ch>
2014-12-11 18:48:46 +0100

    rmse estimation script running correctly

--- musicRecommendation/testing/RMSE_estimate.m
+++ musicRecommendation/testing/RMSE_estimate.m
@@ -8,7 +8,7 @@ load songTrain;
 MAX_ITER=100; %maximum number of iterations 
 K=10; % #of folds used for cross validation 
 % These 2 parameters are fixed by the cross validation procedure before we come to testing 
-lambda_star=35.83; 
+lambda_star=0.6; 
 Nf_star = 35;
 
 
@@ -27,8 +27,8 @@ for kk=1:K
     % now prepare for prediction get rid of the data processing we made
     % before learning the model :
     Y_pred= U'*A; % compute the prediction for the known users-artisrs
-    Y_pred=(Y_pred+y_mu)*y_std; % denormalize 
-    Y_pred=exp(Y_pred)-1; % the inverse transformation
+    Y_pred=Y_pred*y_std+y_mu; % denormalize 
+    Y_pred=10.^(Y_pred)-1; % the inverse transformation
     n_users=size(Y_pred,1); 
     Y_pred=insertrows(Y_pred',zeros(length(I),n_users),I);% we put back the artists we removed earlier.
     Y_pred=Y_pred';
@@ -37,7 +37,17 @@ for kk=1:K
     subRMSE(2,kk) = cost_func_testing(Ytest_weak,Y_pred);%,I);
     % compute the RMSE for the strong generalization 
     % Note that the prediction for strong relies on heuristic 
-    
-    
+    [i,j,s]=find(Ytest_weak);
+    s_pred=zeros(length(i),1);
+    for k=1:length(i)
+        s_pred(k)=Y_pred(i(k),j(k));
+    end
+    figure
+    scatter(s,s_pred);
+    %title ('mean test rmse %f',subRMSE(2,kk));
+    xlabel('Ytest wek')
+    ylabel('Y pred')
 end
+
+figure 
 boxplot(subRMSE(2,:));
